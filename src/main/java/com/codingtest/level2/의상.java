6e75f8e66b46d0195ee088517455fa22ff3245a8
sunjaewow <chltjswo789@naver.com>
2026-05-03 14:51:36 +0900

    [Level2] : 의상

---
+++ src/main/java/com/codingtest/level2/의상.java
@@ -0,0 +1,21 @@
+package com.codingtest.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int count = 1;
+
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) +1);
+        }
+
+        for(int value : map.values()){
+            count *= value +1;
+        }
+
+        return count -1;
+    }
+}
