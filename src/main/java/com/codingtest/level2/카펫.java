bf7b42a783a04226e4564c5c7f83bbccaef8fe7b
sunjaewow <chltjswo789@naver.com>
2026-05-07 12:54:17 +0900

    [Level2] : 카펫

---
+++ src/main/java/com/codingtest/level2/카펫.java
@@ -0,0 +1,17 @@
+package com.codingtest.level2;
+
+public class 카펫 {
+    public int[] solution(int brown, int yellow) {
+        int sum = (brown - 4)/2;
+
+        for(int i = 1; i*i <= yellow; i++){
+            if(yellow % i == 0){
+                int j = yellow/i;
+                if(i + j == sum){
+                    return new int[]{j+2, i+2};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
