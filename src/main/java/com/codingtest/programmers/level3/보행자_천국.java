d441ec56e10c05e39d64b466a19fde168ac93b8f
sunjaewow <chltjswo789@naver.com>
2026-07-31 19:38:18 +0900

    [Level2] : 롤_케이크_자르기

--- src/main/java/com/codingtest/programmers/level3/보행자_천국.java
+++ src/main/java/com/codingtest/programmers/level3/보행자_천국.java
@@ -1,11 +1,13 @@
 package com.codingtest.programmers.level3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Link : https://school.programmers.co.kr/learn/courses/30/lessons/1832
 public class 보행자_천국 {
     int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
         int[][][] dp = new int[m][n][2];
-
         dp[0][0][0] = 1;
 
         for(int i =0; i<m; i++){
